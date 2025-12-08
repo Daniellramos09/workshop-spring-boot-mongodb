503e67f7d19f0a179639998c5f349e60114bd9e2
Daniel Expedito <danielexpeditospr2@gmail.com>
2025-12-08 12:48:50 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/nelioalves/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/nelioalves/workshopmongo/WorkshopmongoApplication.java
@@ -1,10 +1,16 @@
 package com.nelioalves.workshopmongo;
 
+import com.nelioalves.workshopmongo.domain.Post;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class WorkshopmongoApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopmongoApplication.class, args);
